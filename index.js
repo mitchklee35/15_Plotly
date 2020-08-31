46ccb306fe4f96eb52ffd520147da889d116b35c
Mitch Klee <mithcklee35@gmail.com>
2020-08-31 14:55:50 -0500

    make it better

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// Heroku Port
-var PORT = process.en.PORT || 5000;
+const express = require("express");
+var app = express();
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/data/json", (req, res) => {
+    res.send(importData);
+});
+
+app.listen(port, () => {
+    console.log('App is listening on port http://localhost: ${port}');
+});
